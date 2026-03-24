ecec437309bf6a96c5c45bae1119b9c0698aeb45
Marcus Grönnå <mgronna@gmail.com>
2026-03-24 14:23:15 +0100

    add: consumer

--- Ff7.Consumer/Program.cs
+++ Ff7.Consumer/Program.cs
@@ -1,1 +1,56 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Ff7.Contracts;
+using System.Text.Json;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "ff7-battle-logger",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    EnableAutoCommit = false
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config)
+    .SetErrorHandler((_, error) =>
+    {
+        Console.WriteLine($"Kafka error: {error.Reason}");
+    })
+    .Build();
+
+consumer.Subscribe("ff7.battle-events");
+
+Console.WriteLine("FF7 consumer is running. Waiting for battle events...");
+
+try
+{
+    while (true)
+    {
+        var result = consumer.Consume(CancellationToken.None);
+
+        var battleEvent = JsonSerializer.Deserialize<BattleEvent>(result.Message.Value);
+
+        if (battleEvent is null)
+        {
+            Console.WriteLine("Skipping invalid payload.");
+            continue;
+        }
+
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine($"Topic: {result.Topic}");
+        Console.WriteLine($"Partition: {result.Partition.Value}");
+        Console.WriteLine($"Offset: {result.Offset.Value}");
+        Console.WriteLine($"Key (BattleId): {result.Message.Key}");
+        Console.WriteLine(
+            $"{battleEvent.CharacterName} uses {battleEvent.Action} on {battleEvent.Target}" +
+            $"in battle {battleEvent.BattleId} at {battleEvent.OccurredAtUtc:0}");
+
+        consumer.Commit(result);
+    }
+}
+catch (OperationCanceledException)
+{
+}
+finally
+{
+    consumer.Close();
+}
